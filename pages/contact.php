960ad5059642b76aee1ea9d68984a398b634d1b6
= <trochain.dylan@gmail.com>
2016-11-20 15:23:14 +0100

    chg navbar

--- pages/contact.php
+++ pages/contact.php
@@ -59,24 +59,32 @@
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
-        <!-- Add your site or application content here -->
+        <!-- Navbar fixed top -->
+
         <nav class="navbar navbar-default navbar-fixed-top">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-md-offset-1 col-md-1">
-                        <a class="navbar-brand" href="../index.html"><i class="fa fa-home"></i></a>
-                    </div>
-                    <div class="col-md-offset-4 col-md-6">
-                        <ul class="nav nav-tabs nav-border-colorcenter-block" id="myTab">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="../index.html"><i class="fa fa-home"></i></a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav nav-tabs navbar-right" id="myTab">
                             <li><a href="about.html">About</a></li>
                             <li><a href="formation.html">Formation</a></li>
                             <li><a href="projects.html">Projects</a></li>
                             <li><a href="skills.html">Skills</a></li>
                             <li class="active"><a data-toggle="tab" href="contact.php">Contact</a></li>
                         </ul>
-                    </div>
-                </div>
-            </div>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
         </nav>
 
 
