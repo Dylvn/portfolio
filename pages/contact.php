08181e9853f7093ba362d6eb93fff183eb26d237
= <trochain.dylan@gmail.com>
2016-11-21 20:06:47 +0100

    img

--- pages/contact.php
+++ pages/contact.php
@@ -106,11 +106,13 @@
             <div class="container" id="contact-page">
             <!-- CONTENT -->
             <div class="row">
-                <div class="col-md-5">
+                <div class="col-md-4 col-xs-12">
                       <!-- MAPS -->
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2783.1742100724227!2d4.887650015629318!3d45.76769882116144!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4c036c89784f3%3A0xb6b1722dc00a5225!2sFlachet!5e0!3m2!1sfr!2sfr!4v1479308280112" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2783.1742100724227!2d4.887650015629318!3d45.76769882116144!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4c036c89784f3%3A0xb6b1722dc00a5225!2sFlachet!5e0!3m2!1sfr!2sfr!4v1479308280112" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    </div>
                 </div>
-                <div class="col-md-7" id="form-contact">
+                <div class="col-md-8 col-xs-12" id="form-contact">
                     <h2 class="text-center">Formulaire de contact</h2>
                     <form action="" method="post">
                         <label for="name">Votre nom :</label>
